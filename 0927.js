ce0888e6098b17bb0e8a4a9b49e2f677b1b4194d
youngju612 <146154116+youngju612@users.noreply.github.com>
2023-09-27 12:12:56 +0900

    Add files via upload

---
+++ 0927.js
@@ -0,0 +1,36 @@
+console.log('my'+'cat');
+console.log('10'+2);
+
+console.log("ellie's book");
+
+console.log(1+1);
+console.log(1-1);
+console.log(2/1);
+console.log(4*1);
+console.log(5%2);
+console.log(2**3);
+
+let counter = 2;
+const prei = ++counter;
+console.log('prei: ${prei}, counter: ${counter}');
+const post = counter++;
+console.log('prei: ${prei}, counter: ${counter}');
+const prd = --counter;
+console.log('pred: ${pred}, counter: ${counter}');
+const pred = counter--;
+console.log('pred: ${pred}, counter: ${counter}');
+
+console.log(10<6);
+console.log(10>6);
+
+const value1 = true;
+const value2 = 4 < 2;
+
+const dtringfive = 5.0;
+const numberfive = 5;
+
+console.log(stringfive == numberfive);
+console.log(stringfive != numberfive);
+
+console.log(stringfive === numberfive);
+console.log(stringfive !== numberfive);
