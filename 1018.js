27d58f62fab38341684773c7cea4c12e1cad45ef
youngju612 <146154116+youngju612@users.noreply.github.com>
2023-10-18 12:29:57 +0900

    Update 1018.js

--- 1018.js
+++ 1018.js
@@ -4,6 +4,39 @@ function printHello(){
 console.log('Goodmorning');
 printHello();
 
+function log(message) {
+    console.log(message);
+}
+log('Hello@');
+log(1234);
+
+
+function changeName(obj) {
+    obj.name = 'coder';
+}
+const ellie = {name: 'ellie'};
+changeName(ellie);
+console.log(ellie);
+
+
+function showMessage(message, from = 'unknown') {
+    console.log(`${message} by {from}`);
+}
+showMessage('Hi!')
+
+
+function printAll(...args) {
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+
+    for (const arg of args) {
+        console.log(arg);
+    }
+}
+printAll('dream', 'coding', 'ellie');
+
+
 let globalMessage = 'global';
 function printMessage() {
     let message = 'Hello';
@@ -12,8 +45,9 @@ function printMessage() {
     function printAnother(){
         console.log(message);
         let childmessage = 'hello';
+        console.log(childmessage);
     }
-    console.log(childmessage);
+    printAnother();
 }
 printMessage();
 
